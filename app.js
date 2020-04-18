61630b509221c9756dad9e419f17be49157dc0a1
ctownsdin <ctownsdin@expedia.com>
2020-04-18 10:14:36 -0700

    linting with eslint and prettier

--- app.js
+++ app.js
@@ -1,100 +1,100 @@
-var config = require('./config/config');
-
-// Include the cluster module
-var cluster = require('cluster');
+/* eslint-disable global-require */
+const cluster = require("cluster");
+const config = require("./config/config");
 
 // Code to run if we're in the master process
 if (cluster.isMaster) {
+  // Count the machine's CPUs
+  const cpuCount = require("os").cpus().length;
 
-    // Count the machine's CPUs
-    var cpuCount = require('os').cpus().length;
+  // Create a worker for each CPU
+  for (let i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+  }
 
-    // Create a worker for each CPU
-    for (var i = 0; i < cpuCount; i += 1) {
-        cluster.fork();
-    }
+  // Listen for terminating workers
+  cluster.on("exit", (worker) => {
+    // Replace the terminated workers
+    console.log(`Worker ${worker.id} died :(`);
+    cluster.fork();
+  });
 
-    // Listen for terminating workers
-    cluster.on('exit', function (worker) {
-
-        // Replace the terminated workers
-        console.log('Worker ' + worker.id + ' died :(');
-        cluster.fork();
-
-    });
-
-// Code to run if we're in a worker process
+  // Code to run if we're in a worker process
 } else {
-    var AWS = require('aws-sdk');
-    var express = require('express');
-    var bodyParser = require('body-parser');
+  const AWS = require("aws-sdk");
+  const express = require("express");
+  const bodyParser = require("body-parser");
 
-    AWS.config.region = process.env.REGION
+  AWS.config.region = process.env.REGION;
 
-    var sns = new AWS.SNS();
-    var ddb = new AWS.DynamoDB();
+  const sns = new AWS.SNS();
+  const ddb = new AWS.DynamoDB();
 
-    var ddbTable =  process.env.STARTUP_SIGNUP_TABLE;
-    var snsTopic =  process.env.NEW_SIGNUP_TOPIC;
-    var app = express();
+  const ddbTable = process.env.STARTUP_SIGNUP_TABLE;
+  const snsTopic = process.env.NEW_SIGNUP_TOPIC;
+  const app = express();
 
-    app.set('port', config.port);
+  app.set("port", config.port);
 
-    app.set('view engine', 'ejs');
-    app.set('views', __dirname + '/views');
-    app.use(bodyParser.urlencoded({extended:false}));
+  app.set("view engine", "ejs");
+  app.set("views", `${__dirname}/views`);
+  app.use(bodyParser.urlencoded({ extended: false }));
 
-    app.get('/', function(req, res) {
-        // EJS render index.ejs
-        res.render('index', {
-            static_path: 'static',
-            theme: process.env.THEME || 'flatly',
-            flask_debug: process.env.FLASK_DEBUG || 'false'
-        });
+  app.get("/", (req, res) => {
+    // EJS render index.ejs
+    res.render("index", {
+      static_path: "static",
+      theme: process.env.THEME || "flatly",
+      flask_debug: process.env.FLASK_DEBUG || "false",
     });
+  });
 
-    app.post('/signup', function(req, res) {
-        var item = {
-            'email': {'S': req.body.email},
-            'name': {'S': req.body.name},
-            'preview': {'S': req.body.previewAccess},
-            'theme': {'S': req.body.theme}
-        };
+  app.post("/signup", (req, res) => {
+    const item = {
+      email: { S: req.body.email },
+      name: { S: req.body.name },
+      preview: { S: req.body.previewAccess },
+      theme: { S: req.body.theme },
+    };
 
-        ddb.putItem({
-            'TableName': ddbTable,
-            'Item': item,
-            'Expected': { email: { Exists: false } }        
-        }, function(err, data) {
-            if (err) {
-                var returnStatus = 500;
+    ddb.putItem(
+      {
+        TableName: ddbTable,
+        Item: item,
+        Expected: { email: { Exists: false } },
+      },
+      (err, data) => {
+        if (err) {
+          let returnStatus = 500;
 
-                if (err.code === 'ConditionalCheckFailedException') {
-                    returnStatus = 409;
-                }
+          if (err.code === "ConditionalCheckFailedException") {
+            returnStatus = 409;
+          }
 
-                res.status(returnStatus).end();
-                console.log('DDB Error: ' + err);
-            } else {
-                sns.publish({
-                    'Message': 'Name: ' + req.body.name + "\r\nEmail: " + req.body.email 
-                                        + "\r\nPreviewAccess: " + req.body.previewAccess 
-                                        + "\r\nTheme: " + req.body.theme,
-                    'Subject': 'New user sign up!!!',
-                    'TopicArn': snsTopic
-                }, function(err, data) {
-                    if (err) {
-                        res.status(500).end();
-                        console.log('SNS Error: ' + err);
-                    } else {
-                        res.status(201).end();
-                    }
-                });            
+          res.status(returnStatus).end();
+          console.log(`DDB Error: ${err}`);
+        } else {
+          sns.publish(
+            {
+              Message: `Name: ${req.body.name}\r\nEmail: ${req.body.email}\r\nPreviewAccess: ${req.body.previewAccess}\r\nTheme: ${req.body.theme}`,
+              Subject: "New user sign up!!!",
+              TopicArn: snsTopic,
+            },
+            (err, data) => {
+              if (err) {
+                res.status(500).end();
+                console.log(`SNS Error: ${err}`);
+              } else {
+                res.status(201).end();
+              }
             }
-        });
-    });
+          );
+        }
+      }
+    );
+  });
 
-    var server = app.listen(config.port, function () {
-        console.log('Server running at http://127.0.0.1:' + config.port + '/');
-    });
+  app.listen(config.port, () => {
+    console.log(`Server running at http://127.0.0.1:${config.port}/`);
+  });
 }
