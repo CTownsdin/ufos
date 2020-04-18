f8dfc37e6f7ee83de4db0a5165220ba8505d17d5
ctownsdin <ctownsdin@expedia.com>
2020-04-18 16:02:28 -0700

    use moment to standardize the time strings for better searching later

--- dynamo-operations/populate-dynamo-table.js
+++ dynamo-operations/populate-dynamo-table.js
@@ -1,4 +1,5 @@
 const AWS = require("aws-sdk");
+const moment = require("moment");
 const config = require("../config/config");
 
 AWS.config.update({
@@ -13,25 +14,25 @@ console.log("Importing test UFOs into DB");
 const testUFOs = [
   {
     ID: "154/S154900.html",
-    Date_Time: "4/9/20 16:00",
+    Date_Time: moment("4/9/20 16:00", "MM-DD-YY HH:mm").format(),
     City: "North Stonington",
     State: "CT",
     Shape: "Sphere",
     Duration: "3 Hours",
     Summary:
       "((HOAX??)) going to take out the garbage saw the ball of light flying towards me I ran back inside I thought I was going to be abducted",
-    Posted_Date: "4/9/20",
+    Posted_Date: moment("4/9/20", "MM-DD-YY").format(),
   },
   {
     ID: "154/_TEST_ONLY.html",
-    Date_Time: "4/10/20 18:00",
+    Date_Time: moment("4/10/20 18:00", "MM-DD-YY HH:mm").format(),
     City: "Seattle",
     State: "WA",
     Shape: "Rhombus",
     Duration: "1 Hours",
     Summary:
       "I was watching HBO Max, when this crazy light appeared outside my window...",
-    Posted_Date: "4/19/20",
+    Posted_Date: moment("4/19/20", "MM-DD-YY").format(),
   },
 ];
 
