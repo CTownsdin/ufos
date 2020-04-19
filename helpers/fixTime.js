5c0d83cab726c1490ef715fff3b758e7a19ca778
ctownsdin <ctownsdin@expedia.com>
2020-04-19 13:24:07 -0700

    add a few more cases to fixTime helper

--- helpers/fixTime.js
+++ helpers/fixTime.js
@@ -3,46 +3,52 @@ const moment = require("moment");
 // Deal with normalizing different time formats
 // handy: https://regex101.com/ enjoy!
 
-// "2016-07-04 21:00:00",
-// ^\d{4}-\d{1,2}-\d{1,2}\s\d{1,2}:\d{1,2}:\d{1,2}$
-
-// "2016-07-04 21:00",
-// ^\d{4}-\d{1,2}-\d{1,2}\s\d{1,2}:\d{1,2}$
-
-// "4/15/16 22:15",
-// ^\d{1,2}\/\d{1,2}\/\d{2}\s\d{1,2}:\d{1,2}$
-
-// hypothetical 4th, just in case
-// 4/15/2020 22:15
-// ^\d{1,2}\/\d{1,2}\/\d{4}\s\d{1,2}:\d{1,2}$
-
-// hypothetical 5th, just in case
-// 4/15/2020 22:15:00
-// ^\d{1,2}\/\d{1,2}\/\d{4}\s\d{1,2}:\d{1,2}:\d{1,2}$
-
 function fixTime(input) {
+  // "2016-07-04 21:00:00",
+  // ^\d{4}-\d{1,2}-\d{1,2}\s\d{1,2}:\d{1,2}:\d{1,2}$
   if (/^\d{4}-\d{1,2}-\d{1,2}\s\d{1,2}:\d{1,2}:\d{1,2}$/.test(input)) {
     return moment(input, "YYYY-MM-DD HH:mm:ss").format();
   }
 
+  // "2016-07-04 21:00",
+  // ^\d{4}-\d{1,2}-\d{1,2}\s\d{1,2}:\d{1,2}$
   if (/^\d{4}-\d{1,2}-\d{1,2}\s\d{1,2}:\d{1,2}$/.test(input)) {
     return moment(input, "YYYY-MM-DD HH:mm").format();
   }
 
+  // "4/15/16 22:15",
+  // ^\d{1,2}\/\d{1,2}\/\d{2}\s\d{1,2}:\d{1,2}$
   if (/^\d{1,2}\/\d{1,2}\/\d{2}\s\d{1,2}:\d{1,2}$/.test(input)) {
     return moment(input, "MM-DD-YY HH:mm").format();
   }
 
+  // "4/15/2020 22:15"
+  // ^\d{1,2}\/\d{1,2}\/\d{4}\s\d{1,2}:\d{1,2}$
   if (/^\d{1,2}\/\d{1,2}\/\d{4}\s\d{1,2}:\d{1,2}$/.test(input)) {
     return moment(input, "MM-DD-YYYY HH:mm").format();
   }
 
+  // "4/15/2020 22:15:00"
+  // ^\d{1,2}\/\d{1,2}\/\d{4}\s\d{1,2}:\d{1,2}:\d{1,2}$
   if (/^\d{1,2}\/\d{1,2}\/\d{4}\s\d{1,2}:\d{1,2}:\d{1,2}$/.test(input)) {
     return moment(input, "MM-DD-YYYY HH:mm:ss").format();
   }
 
+  // "11/13/16"
+  // ^\d{1,2}\/\d{1,2}\/\d{2}$
+  if (/^\d{1,2}\/\d{1,2}\/\d{2}$/.test(input)) {
+    return moment(input, "MM-DD-YY").format();
+  }
+
+  // "11/13/2020"
+  // ^\d{1,2}\/\d{1,2}\/\d{4}$
+  if (/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(input)) {
+    return moment(input, "MM-DD-YYYY").format();
+  }
+
   console.error(`Unmatched Time Format: ${input}`);
-  return "Invalid date"; // Note: Sometimes it is a location, and not a date. TODO: Do something?
+  throw new Error(`Unmatched Time Format: ${input}`);
+  // Note: Sometimes it is a location, and not a date. Bad Data.
 }
 
 module.exports = fixTime;
