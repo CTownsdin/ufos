fe253474e4da6b6e7dab570fa7e21de72e4952cb
ctownsdin <ctownsdin@expedia.com>
2020-04-19 08:39:33 -0700

    tweak db schema and start populating dev DB from the large ufos.js file

--- dynamo-operations/populate-dynamo-table.js
+++ dynamo-operations/populate-dynamo-table.js
@@ -1,52 +1,34 @@
 const AWS = require("aws-sdk");
 const moment = require("moment");
 const config = require("../config/config");
+const bigUfos = require("../data/ufos");
 
 AWS.config.update({
   region: "us-west-2",
-  endpoint: config.dynamo_endpoint,
+  endpoint: "https://dynamodb.us-west-2.amazonaws.com",
 });
 
 const docClient = new AWS.DynamoDB.DocumentClient();
-
 console.log("Importing test UFOs into DB");
 
-const testUFOs = [
-  {
-    ID: "154/S154900.html",
-    Date_Time: moment("4/9/20 16:00", "MM-DD-YY HH:mm").format(),
-    City: "North Stonington",
-    State: "CT",
-    Shape: "Sphere",
-    Duration: "3 Hours",
-    Summary:
-      "((HOAX??)) going to take out the garbage saw the ball of light flying towards me I ran back inside I thought I was going to be abducted",
-    Posted_Date: moment("4/9/20", "MM-DD-YY").format(),
-  },
-  {
-    ID: "154/_TEST_ONLY.html",
-    Date_Time: moment("4/10/20 18:00", "MM-DD-YY HH:mm").format(),
-    City: "Seattle",
-    State: "WA",
-    Shape: "Rhombus",
-    Duration: "1 Hours",
-    Summary:
-      "I was watching HBO Max, when this crazy light appeared outside my window...",
-    Posted_Date: moment("4/19/20", "MM-DD-YY").format(),
-  },
-];
+bigUfos.slice(0, 3).forEach((ufo) => {
+  const newUfo = ufo;
 
-testUFOs.forEach((ufo) => {
+  newUfo["Date / Time"] = moment(
+    ufo.Occurrence_Date_Time,
+    "MM-DD-YY HH:mm"
+  ).format();
+
   const params = {
     TableName: "ufos",
-    Item: ufo,
+    Item: newUfo,
   };
 
   docClient.put(params, (err, data) => {
     if (err) {
       console.error(`Unable to add ufo ${JSON.stringify(err, null, 2)}`);
     } else {
-      console.log("PutItem success", ufo.Date_Time, ufo.City, ufo.State);
+      console.log(`PutItem success: ${ufo["Date / Time"]} ${ufo.City}`);
     }
   });
 });
